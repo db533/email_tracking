bff33f52e56e058957b9c942b5283a1143d36264
Dainis Berzins <db5331@gmail.com>
2023-01-26 17:00:34 +0200

    Implementing django_environ module

--- copy_to_prod.sh
+++ copy_to_prod.sh
@@ -7,7 +7,7 @@ src_dir="/home/saknesar/dundlabumi.lv/dainis_git_action"
 dst_dir="/home/saknesar/email/"
 
 # array of files and directories to copy
-files_to_copy=("manage.py" "passenger_wsgi.py" "prepare_virtual_env.sh" "email_tracking" "tracking_app")
+files_to_copy=("manage.py" "passenger_wsgi.py" "prepare_virtual_env.sh" "requirements.txt" "email_tracking" "tracking_app")
 
 # loop through the array and copy each file/folder
 for item in "${files_to_copy[@]}"; do
