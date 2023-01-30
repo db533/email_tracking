ca7589c84e421e58f58631d7f2b932152ecdf8ec
Dainis Berzins <db5331@gmail.com>
2023-01-30 18:25:33 +0200

    Trying to configure REST API for email sending

--- tracking_app/views.py
+++ tracking_app/views.py
@@ -16,36 +16,6 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
-#https://alwaysdjango.com/how-to-send-html-emails-in-django/
-
-from django.conf import settings
-from django.http import HttpResponse
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
-from django.utils.html import strip_tags
-
-def sendHTMLEmail(request):
-    context ={
-        "title":"Test",
-        "content":"Testing sending HTML emails from Django"
-    }
-    html_content = render_to_string("mail_template.html", context)
-    text_content = strip_tags(html_content)
-    thread = threading.Thread(target=emailSender, args=("Email Subject", text_content,html_content,['db5331@gmail.com']))
-    thread.start()
-    return HttpResponse("Email Sent successfully")
-
-
-def emailSender(subject, text_content,html_content, reciepants):
-    email = EmailMultiAlternatives(
-        subject,
-        text_content,
-        settings.EMAIL_HOST_USER ,
-        reciepants
-    )
-    email.attach_alternative(html_content, 'text/html')
-    email.send()
-
 # Adding serializers for API usage in REST framework
 # https://www.django-rest-framework.org/tutorial/quickstart/
 from django.contrib.auth.models import User, Group
@@ -90,7 +60,7 @@ class SendTemplateMailView(APIView):
         url_is = context_data_is["image_url"]
         context_data_is['url_is'] = url_is
         html_detail = mail_template.render(context_data_is)
-        subject, from_email, to = "Greetings !!", 'postmaster@manojadhikary.com.np', [target_user_email]
+        subject, from_email, to = "Greetings !!", 'info@dundlabumi.lv', [target_user_email]
         msg = EmailMultiAlternatives(subject, html_detail, from_email, to)
         msg.content_subtype = 'html'
         msg.send()
