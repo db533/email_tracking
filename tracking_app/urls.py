09d87c750fcab8aeabae23c7aa0374520e70935f
Dainis Berzins <db5331@gmail.com>
2023-02-02 16:20:42 +0200

    Changing url patterns, excluding original patterns, leaving only for tracking

--- tracking_app/urls.py
+++ tracking_app/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
 from . import views
 
-urlpatterns = [
-    path('index/', views.index, name='index'),
-]
+#urlpatterns = [
+#    path('index/', views.index, name='index'),
+#]
 
 # Use static() to add URL mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # Adding serializers for API usage in REST framework
 # https://www.django-rest-framework.org/tutorial/quickstart/
@@ -21,7 +21,7 @@ router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns += [
+urlpatterns = [
     path('api/', include(router.urls)),
 ]
 
