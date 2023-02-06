d84f4e54b366b6609db8ab746f7299ff96cc773d
Dainis Berzins <db5331@gmail.com>
2023-02-06 15:31:51 +0200

    Added new models and view for link tracking

--- tracking_app/views.py
+++ tracking_app/views.py
@@ -181,7 +181,7 @@ from django.shortcuts import redirect
 def link(request, id):
     redirect = Redirect.objects.get(redirect_code=id)
     target_url=Redirect.target_url
-    redirect.save()
+    #redirect.save()
     click = Click.objects.create(redirect_code=redirect.id)
     return redirect(target_url)
 
