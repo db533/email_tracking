31398711eee4ab4aadb59dd20ac46af1b36c8adb
Dainis Berzins <db5331@gmail.com>
2023-02-06 13:58:16 +0200

    Added new models and view for link tracking

--- tracking_app/admin.py
+++ tracking_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Email, UserModel
+from .models import Email, UserModel, Redirect, Click, OutboundEmail
 
 # Register your models here.
 admin.site.register(Email)
 admin.site.register(UserModel)
+admin.site.register(Redirect)
+admin.site.register(Click)
+admin.site.register(OutboundEmail)
