0a1e8a7dae3633885333508477a863af2c650771
Dainis Berzins <db5331@gmail.com>
2023-02-16 12:32:01 +0200

    Trying to resolve null session.

--- tracking_app/views.py
+++ tracking_app/views.py
@@ -198,14 +198,14 @@ def page(request, id):
     # Get the session from the received request
     if request.session.has_key('session_key'):
         session_key = request.session['session_key']
-        session_key = "From cookie"
+        #session_key = "From cookie"
     if not request.session.has_key('session_key') or session_key is None:
         session_key = request.session.session_key
-        session_key = "Reading from session"
-        if session_key is None:
-            request.session.create()
-            session_key = request.session.session_key
-            session_key = "Created session reading from session"
+        #session_key = "Reading from session"
+        #if session_key is None:
+        request.session.create()
+        session_key = request.session.session_key
+        #session_key = "Created session reading from session"
         #request.session['session_key'] = session_key
     session, created = Session.objects.update_or_create(session_key=session_key)
     pageview = Pageview.objects.create(page=id, session_key=session_key, session=session)
