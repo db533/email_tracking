c442db64b3de508092115bb6257709d755b1c2f0
Dainis Berzins <db5331@gmail.com>
2023-02-27 12:50:19 +0200

    Adding Subscriber foreign key to Redirect model. Updating UserLink creation code.

--- tracking_app/views.py
+++ tracking_app/views.py
@@ -294,7 +294,7 @@ def link(request, id):
     click = Click.objects.create(redirect_code_id=id, session_key=session_key, session=session, temp_message = temp_message)
 
     # Now increment the User / Link relevance score.
-    clicked_wpid = WPID.objects.get(wp_id=id)
+    clicked_wpid = WPID.objects.get(wp_id=redirect_record.wpid)
     if UserLink.objects.filter(user_model=subscriber, wpid=clicked_wpid).exists():
         # Already have a relevance score for this link, so it has been clicked in the last 2 years
         user_link=UserLink.objects.get(user_model=subscriber, wpid=clicked_wpid)
